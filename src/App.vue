e4dc2997c516281d99b6df7601da46b833ac3260
Md. Faizur Rahman Khan <faiz@sayburgh.com>
2024-09-12 15:39:12 +0600

    coded a todo app in vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>My To-Do Lists</h1>
+    <input
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+      placeholder="Add a new task"
+    />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <span :class="{ completed: todo.completed }" @click="toggleTodo(index)">
+          {{ todo.text }}
+        </span>
+        <button @click="removeTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== "") {
+        this.todos.push({
+          text: this.newTodo,
+          completed: false,
+        });
+        this.newTodo = "";
+      }
+    },
+    toggleTodo(index) {
+      this.todos[index].completed = !this.todos[index].completed;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+}
+
+button {
+  margin-left: 10px;
+  background-color: red;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: darkred;
 }
 </style>
